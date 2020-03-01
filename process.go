fd1c3ce1973dd08454973523d0ee5cd9b26af972
Markus Blaschke <mblaschke82@gmail.com>
2020-03-01 18:39:07 +0100

    bugfix
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- process.go
+++ process.go
@@ -120,7 +120,7 @@ func probeCollect() {
 					}).Set(eventValue)
 
 				if opts.VmNodeName != "" && resource == opts.VmNodeName {
-					Logger.Println(fmt.Sprintf("detected %v in %v seconds", event.EventType, time.Unix(int64(eventValue), 0).Sub(time.Now()).String()))
+					Logger.Println(fmt.Sprintf("detected %v in %v", event.EventType, time.Unix(int64(eventValue), 0).Sub(time.Now()).String()))
 					if eventValue == 1 || drainTimeThreshold >= eventValue {
 						switch strings.ToLower(event.EventType) {
 						case "reboot":
